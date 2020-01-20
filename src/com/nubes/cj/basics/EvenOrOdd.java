14833fbea53d990a30b7817747819a333a030717
SriajayS <sanipinigangadhar143@gmail.com>
2020-01-20 19:36:51 +0530

    added even or odd program

---
+++ src/com/nubes/cj/basics/EvenOrOdd.java
@@ -0,0 +1,13 @@
+package com.nubes.cj.basics;
+
+public class EvenOrOdd {
+
+	public static void main(String[] args) {
+		int num = 25;
+		if(num % 2 == 0) 
+			System.out.println("Even number");
+		else
+			System.out.println("Odd Number");
+	}
+
+}
