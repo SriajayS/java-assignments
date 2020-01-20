9bce5fb7573ecd8f9be7586675c8bf38927af9c1
SriajayS <sanipinigangadhar143@gmail.com>
2020-01-20 19:51:54 +0530

    Added new Java Programs

--- src/com/nubes/cj/basics/EvenOrOdd.java
+++ src/com/nubes/cj/basics/EvenOrOdd.java
@@ -3,11 +3,7 @@ package com.nubes.cj.basics;
 public class EvenOrOdd {
 
 	public static void main(String[] args) {
-		int num = 25;
-		if(num % 2 == 0) 
-			System.out.println("Even number");
-		else
-			System.out.println("Odd Number");
+		System.out.println(20+30+20+" ,");
 	}
 
 }
---
+++ src/com/nubes/cj/basics/TestDemo.java
@@ -0,0 +1,9 @@
+package com.nubes.cj.basics;
+
+public class TestDemo {
+
+	public static void main(String[] args) {
+		System.out.println("This is testing class for demo");
+	}
+
+}
